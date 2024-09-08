4a0c28daa23c40104f072bdf889cbdb3e25e688d
MrFosters <imre.ivan@outlook.com>
2024-09-08 03:39:23 +0200

    Update MayaTIN.py
    Changed: started implementing a GUI

--- MayaTIN.py
+++ MayaTIN.py
@@ -12,19 +12,59 @@
 ##
 
 import maya.cmds as cmds
-import os
+import maya.mel as mel
+from maya import OpenMayaUI as omui
+from shiboken6 import wrapInstance
+from PySide6 import QtUiTools, QtCore, QtGui, QtWidgets
+from functools import partial # optional, for passing args during signal function calls
+import sys
 
 ################################
 #    initialize
 ################################
 
-fileFolder = "replace\\these\\paths\\with\\your\\location\\"
-#fileFolder = "replace\\these\\paths\\with\\your\\location\\testfolder\\"
+#fileFolder = "replace\\these\\paths\\with\\your\\location\\"
+fileFolder = str()
 fileCSV = fileFolder+"\\crashSafe.csv"
 tileArray = []
 tileName = []
 currentTile = str()
 reductionAmount = 96
+
+################################
+
+class tin_tool(QtWidgets.QWidget):
+    window = None
+    
+    def __init__(self, parent = None):
+        """
+        Initialize class.
+        """
+        super(tin_tool, self).__init__(parent = parent)
+        self.setWindowFlags(QtCore.Qt.Window)
+        self.widgetPath = ("C:\\Users\\Imre\\Documents\\TIN_Project\\")
+        self.widget = QtUiTools.QUiLoader().load(self.widgetPath + "TIN.ui")    
+        self.widget.setParent(self)
+        
+        self.check_lod_enable = self.widget.findChild(QtWidgets.QCheckBox, "check_lod_enable")
+        self.check_lod_enable.toggled.connect(self.enable_lod)
+        
+        self.b_add_lod = self.widget.findChild(QtWidgets.QPushButton, "b_add_lod")
+        self.b_remove_lod = self.widget.findChild(QtWidgets.QPushButton, "b_remove_lod")
+        self.lod_table = self.widget.findChild(QtWidgets.QTableWidget, "lod_table")
+        
+
+    def enable_lod(self):
+        check_lod_enable = self.widget.findChild(QtWidgets.QCheckBox)
+        
+        if (check_lod_enable.isChecked()):
+            self.lod_table.setEnabled(True)
+            self.b_add_lod.setEnabled(True)
+            self.b_remove_lod.setEnabled(True)
+        else:
+            self.lod_table.setEnabled(False)
+            self.b_add_lod.setEnabled(False)
+            self.b_remove_lod.setEnabled(False)
 
 ################################
 # Whether to save the processed filenames into a CSV file in case of a crash
@@ -69,6 +109,32 @@ def reduce(newName, filePath):
 def clearScene():
     cmds.file(f=True, newFile=True)
 
-get_tiles(fileFolder)
-parse_tiles()
+    
+def browseFolder(*args):
+    fileFolder = cmds.fileDialog2(fileMode=3, okCaption="Open folder")
+    return(fileFolder)
+
+#get_tiles(fileFolder)
+#parse_tiles()
 #print_tiles()
+
+def createWindow():
+    """
+    ID Maya and attach tool window.
+    """
+    # Maya uses this so it should always return True
+    if QtWidgets.QApplication.instance():
+        # Id any current instances of tool and destroy
+        for win in (QtWidgets.QApplication.allWindows()):
+            if 'myToolWindowName' in win.objectName(): # update this name to match name below
+                win.destroy()
+
+    #QtWidgets.QApplication(sys.argv)
+    mayaMainWindowPtr = omui.MQtUtil.mainWindow()
+    mayaMainWindow = wrapInstance(int(mayaMainWindowPtr), QtWidgets.QWidget)
+    tin_tool.window = tin_tool(parent = mayaMainWindow)
+    tin_tool.window.setObjectName('myToolWindowName') # code above uses this to ID any existing windows
+    tin_tool.window.setWindowTitle("Maya TIN Optimizier")
+    tin_tool.window.show()
+    
+createWindow()
